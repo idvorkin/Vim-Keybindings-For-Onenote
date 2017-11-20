d8fec316e8dd39c244d846614133eeac5a198e33
William <bluedrink9@gmail.com>
2017-11-20 15:48:42 +1300

    Add commandline argument for testing
    Run tests before building, if one fails, don't build.
    
    
    Former-commit-id: c119a047a11f4d843ecdc54f54b3d1c20945938e

--- build.bat
+++ build.bat
@@ -1,3 +1,16 @@
-REM You need intall ahk2exe, this script assumes it is at \bin_drop\ahk2exe
-"C:\Program Files\AutoHotkey\Compiler\ahk2exe.exe" /in vim_onenote.ahk /out vim_onenote.exe
+@echo off
+SET AHKPath=C:\Program Files\AutoHotkey
+REM Takes a sinple optional command parameter, /t, which starts testing.
+SET Param1=%1
 
+if "%Param1%"=="/t" (
+    Start "%AHKPath%\Autohotkey.exe" vim_onenote.ahk 
+    "%AHKPath%\Autohotkey.exe" vim_onenote_testscript.ahk 
+)
+REM Return code from above (0 if tests all pass) is stored in %errorlevel%
+if errorlevel 1 (
+   echo Tests failed. Exe not built.
+   exit /b %errorlevel%
+)
+REM You need intall ahk2exe, this script assumes it is at \bin_drop\ahk2exe
+"%AHKPath%\compiler\ahk2exe.exe" /in vim_onenote.ahk /out vim_onenote.exe
