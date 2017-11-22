5c7778369d4df9d1b693ec5dc38bffa741e9509a
William <bluedrink9@gmail.com>
2017-11-22 13:38:15 +1300

    Change Error output format
    Former-commit-id: 16239a8a962cfd4c6296e6ce9b6db0b5726aaac1

--- build.bat
+++ build.bat
@@ -8,13 +8,15 @@ if "%Param1%"=="/t" (
 )
 REM Return code from above (0 if tests all pass) is stored in %errorlevel%
 if errorlevel 1 (
-   echo Tests failed. Exe not built. Log contents:
-   echo  
-   echo  
-   type testLogs\*
-   echo  
-   echo  
-   exit /b %errorlevel%
+    echo   .
+    echo   .
+    echo Tests failed. Exe not built. Log contents:
+    echo   .
+    echo   .
+    type testLogs\*
+    echo   .
+    echo   .
+    exit /b %errorlevel%
 )
 REM You need intall ahk2exe, this script assumes it is at \bin_drop\ahk2exe
 "%AHKPath%\compiler\ahk2exe.exe" /in vim_onenote.ahk /out vim_onenote.exe
